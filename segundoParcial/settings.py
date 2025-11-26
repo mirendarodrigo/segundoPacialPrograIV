47ad1020e18cf3b292c2f2757f4e30e78dbe934a
mirendarodigo <mirendarodrigo@gmail.com>
2025-11-26 16:42:31 -0300

    config  mail

--- segundoParcial/settings.py
+++ segundoParcial/settings.py
@@ -121,17 +121,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = '/'
 
-# --- EMAIL ---
-if not DEBUG:
-    
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    
-    EMAIL_HOST_USER = env("EMAIL_USER")     
-    EMAIL_HOST_PASSWORD = env("EMAIL_PASS")
-else:
- 
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_HOST_USER = 'test@local.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mirendarodrigo@gmail.com' 
+EMAIL_HOST_PASSWORD = 'qlaj sfsi sgpv qbdi'  
