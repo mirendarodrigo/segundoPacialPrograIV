b8aab27b0854ba5fdfac1ea7dc9f474c75c49fdd
mirendarodigo <mirendarodrigo@gmail.com>
2025-11-26 17:01:28 -0300

    nueva contraseña

--- segundoParcial/settings.py
+++ segundoParcial/settings.py
@@ -126,4 +126,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mirendarodrigo@gmail.com' 
-EMAIL_HOST_PASSWORD = 'qlaj sfsi sgpv qbdi'  
+EMAIL_HOST_PASSWORD = 'pdxw txoq cegw plja'
